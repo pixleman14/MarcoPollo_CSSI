6557dbe8c5ab326f9ae8d3bcc394cacbf8247cdb
pixleman14 <pixleman14@users.noreply.github.com>
2018-08-13 09:38:23 -0700

    Basic-Web=Struct-v2

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Script Connected");
+
+document.getElementById('btn1').onclick = function() {clickFunc1()};
+document.getElementById('btn2').onclick = function() {clickFunc2()};
+document.getElementById('btn3').onclick = function() {clickFunc3()};
+document.getElementById('btn4').onclick = function() {clickFunc4()};
+document.getElementById('btn5').onclick = function() {clickFunc5()};
+function clickFunc1()
+{
+  window.location.href="index.html"
+}
+function clickFunc2()
+{
+  window.location.href="index-2.html"
+}
+function clickFunc3()
+{
+  window.location.href="index-3.html"
+}
+function clickFunc4()
+{
+  window.location.href="index-4.html"
+}
+function clickFunc5()
+{
+  window.location.href="index-5.html"
+}
